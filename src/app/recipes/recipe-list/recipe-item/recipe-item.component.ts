a7a093183e831ce98a9501b4e5e77dbb5899d6cf
Johann Martens <jmartens@conet.de>
2022-07-20 15:47:22 +0200

    added routing, navigation works, bit shopping list add seems to be broken

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,15 +8,8 @@ import { RecipeService } from '../../recipe.service';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input()
-  recipe!: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
-
-  constructor(private recipeService: RecipeService) {}
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  @Input() recipe!: Recipe;
+  @Input() index!: number;
 
 
   ngOnInit(): void {
